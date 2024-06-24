5a55b1cecab42966f884e5b080258eed4c69d2d7
h4kv <h4kv999@gmail.com>
2024-06-24 13:55:52 -0300

    feat: finish base project

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ export function App() {
           <div className='flex gap-2'>
             <a
               href='#'
-              className='hover:text-principal-negative text-slate-500 transition duration-300'
+              className='text-slate-500 transition duration-300 hover:text-principal-negative'
             >
               <Home />
             </a>
             <a
               href='#'
-              className='hover:text-principal-negative text-slate-500 transition duration-300'
+              className='text-slate-500 transition duration-300 hover:text-principal-negative'
             >
               <Images />
             </a>
@@ -23,13 +23,13 @@ export function App() {
           <div className='flex gap-2'>
             <a
               href='#'
-              className='hover:text-principal-negative text-slate-500 transition duration-300'
+              className='text-slate-500 transition duration-300 hover:text-principal-negative'
             >
               <Twitter />
             </a>
             <a
               href='#'
-              className='hover:text-principal-negative text-slate-500 transition duration-300'
+              className='text-slate-500 transition duration-300 hover:text-principal-negative'
             >
               <Mail />
             </a>
@@ -39,11 +39,13 @@ export function App() {
       </header>
       <section>
         <div className='pt-14 text-center'>
-          <h2 className='text-principal-negative font-medium uppercase leading-3 text-xl'>coleções</h2>
+          <h2 className='text-xl font-medium uppercase leading-3 text-principal-negative'>
+            coleções
+          </h2>
           <span className='text-sm text-slate-400'>Principais coleções</span>
         </div>
         <div className='ml-4 pt-14'>
-          <h2 className='text-principal-negative text-sm font-medium uppercase leading-3'>
+          <h2 className='text-sm font-medium uppercase leading-3 text-principal-negative'>
             dungeons & dungeons
           </h2>
           <span className='text-sm text-slate-400'>de onde vem...</span>
@@ -59,13 +61,13 @@ export function App() {
           </div>
         </aside>
         <div className='ml-4 pt-14'>
-          <h2 className='text-principal-negative text-sm font-medium uppercase leading-3'>
+          <h2 className='text-sm font-medium uppercase leading-3 text-principal-negative'>
             arco-iris
           </h2>
           <span className='text-sm text-slate-400'>Uma coleção mais que colorida</span>
         </div>
         <aside className='px-4'>
-          <div className='grid grid-cols-3 max-md:grid-cols-2 gap-2'>
+          <div className='grid grid-cols-3 gap-2 max-md:grid-cols-2'>
             <img src={images.colorized_1} className='w-full' />
             <img src={images.colorized_2} className='w-full' />
             <img src={images.colorized_3} className='w-full' />
@@ -75,7 +77,7 @@ export function App() {
           </div>
         </aside>
         <div className='ml-4 pt-14'>
-          <h2 className='text-principal-negative text-sm font-medium uppercase leading-3'>
+          <h2 className='text-sm font-medium uppercase leading-3 text-principal-negative'>
             Vitral
           </h2>
           <span className='text-sm text-slate-400'>Expressões medievais</span>
@@ -88,6 +90,60 @@ export function App() {
           </div>
         </aside>
       </section>
+      <section>
+        <div className='py-14 text-center'>
+          <h2 className='text-xl font-medium uppercase leading-3 text-principal-negative'>Sobre</h2>
+          <span className='text-sm text-slate-400'>Um pouquinho sobre mim</span>
+        </div>
+        <div className='m-auto w-fit'>
+          <img
+            src={images.profile}
+            alt='profile icon'
+            className='m-auto mb-14 w-96 rounded-full max-md:w-72'
+          />
+          <p className='m-auto mb-7 w-72 text-center'>
+            <span className='font-medium'>Carimbo</span> é um designer de personagens apaixonado por
+            dar vida a ideias através de ilustrações vibrantes.
+          </p>
+        </div>
+        <div className='flex-col gap-16'>
+          <img src={images.sketch_1} alt='' className='w-full' />
+          <img src={images.sketch_2} alt='' className='w-full' />
+          <img src={images.sketch_3} alt='' className='w-full' />
+          <div className='flex'>
+            <img src={images.sketch_4} alt='' className='w-full' />
+            <img src={images.sketch_5} alt='' className='w-full' />
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className='py-14 text-center'>
+          <h2 className='text-xl font-medium uppercase leading-3 text-principal-negative'>
+            Contato
+          </h2>
+          <span className='text-sm text-slate-400'>Entre em contato comigo</span>
+        </div>
+      </section>
+      <section className='mb-7 flex'>
+        <a
+          href='https://x.com/CarimboTinteiro'
+          target='_blank'
+          className='flex h-48 w-full items-center justify-center bg-principal-negative text-principal-orange-200 transition duration-300 hover:bg-principal-purple'
+        >
+          <Twitter />
+        </a>
+
+        <a
+          href='mailto:Carimbotint@gmail.com'
+          target='_blank'
+          className='flex h-48 w-full items-center justify-center bg-principal-negative text-principal-orange-200 transition duration-300 hover:bg-principal-purple'
+        >
+          <Mail />
+        </a>
+      </section>
+      <footer className='py-6'>
+        <p className='text-center text-sm text-slate-400'>© 2024 Carimbo - Designed & Coded by Haku</p>
+      </footer>
     </div>
   );
 }
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,11 @@
 import icon from './icon.webp';
 import banner from './banner.webp';
+import profile from './profile.webp';
+import sketch_1 from './sketch-1.webp';
+import sketch_2 from './sketch-2.webp';
+import sketch_3 from './sketch-3.webp';
+import sketch_4 from './sketch-4.webp';
+import sketch_5 from './sketch-5.webp';
 import principal_1 from './principal-1.webp';
 import principal_2 from './principal-2.webp';
 import principal_3 from './principal-3.webp';
@@ -15,8 +21,14 @@ import strainedGlass_2 from './stainedGlass-2.webp';
 import strainedGlass_3 from './stainedGlass-3.webp';
 
 export const images = {
-  banner,
   icon,
+  banner,
+  profile,
+  sketch_1,
+  sketch_2,
+  sketch_3,
+  sketch_4,
+  sketch_5,
   principal_1,
   principal_2,
   principal_3,
